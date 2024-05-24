2330e3661798fe9c809545e9cc272db73b522057
Code_Hotaru <115505668+Renegade-3863@users.noreply.github.com>
2024-05-24 21:25:39 +0800

    Upload 2024/05/24

--- Chapter1 UnixBasics/myls.c
+++ Chapter1 UnixBasics/myls.c
@@ -1,9 +1,11 @@
 #include "apue.h"
 #include <dirent.h>
+// <dirent.h> 头文件用于调用opendir和readdir函数原型, 以及 dirent 和 DIR 结构的定义
 #include <string.h>
 #include <stdlib.h>
 #include <linux/limits.h>
 #define yearsecs (60 * 60 * 24 * 365)
+// 根据原书提供的ls实现代码魔改而成的类ls -al结果的实现代码, 附功能注释 
 
 int main(int argc, char *argv[]) {
     // DIR为目录流类型, 类似FILE, 用于读取输入的目录/文件流(具体地, 可以用于获取后面的dirent结构)
@@ -18,16 +20,20 @@ int main(int argc, char *argv[]) {
     // 使用opendir(const char*)函数获取对应的目录流, 如果返回流为空, 说明无法在当前目录下找到对应的目录流(权限不够/不存在对应目录名)
     if((dp = opendir(argv[1])) == NULL)
         err_sys("Can't open %s", argv[1]);
-    // 保存原始查询的目录位置, 使用linux/limits.h头文件定义的PATH_MAX路径最大长度变量, 多出一位是用于结尾'\0'
+    // 保存原始查询的目录位置, 使用linux/limits.h头文件定义的PATH_MAX路径最大长度变量, 多出一位是用于结尾'\0', PATH_MAX定义的实际值为4096
     char dir[PATH_MAX+1] = {0x00};
     // 调用C库函数realpath进行用户输入路径名的解析, 规范后续stat读取对应目录
+    // 解析argv[1], 并将解析结果存入dir串
     realpath(argv[1], dir);
-    printf("%s: dir\n", dir);
+    // printf("%s: dir\n", dir);
     while((dirp = readdir(dp)) != NULL) {
+        // 将原始绝对路径目录读入argv[1], 用于stat获取dirsta具体文件信息
         argv[1] = strdup(dir);
+        // 由于需要获取所有当下目录的信息, 所以手动连接绝对路径
         strcat(argv[1], "/");
         strcat(argv[1], dirp->d_name);
         // printf("%s\n", argv[1]);
+        // stat函数将对应目录的具体信息存入dirsta结构体中
         if(stat(argv[1], &dirsta) == -1) {
             printf("%s: ", argv[1]);
             perror("stat error");
